99c05a6fb5483e6729ad982ea041e98de110e89f
Paul <paulmichaelhardman@gmail.com>
2022-03-18 16:10:14 +0000

    all

--- src/Containers/BeerCard/Beercard.jsx
+++ src/Containers/BeerCard/Beercard.jsx
@@ -20,31 +20,41 @@ const Beercard = ({ beer }) => {
         if (loading) return <Loading />;
         return (
           <>
-            {showText ? <div
-              className="beerCard"
-              onMouseEnter={hoveredBeer}
-              onMouseLeave={hoveredBeer}
-              style={{ background: data[1] }}
-              onClick ={handleClick}
-            >
-              <Ticker move={tickerOn} className="ticker">
-                {({ index }) => (
-                  <>
-                    <p>{beer.tagline}</p>
-                  </>
-                )}
-              </Ticker>
+            {showText ? (
+              <div
+                className="beerCard"
+                onMouseEnter={hoveredBeer}
+                onMouseLeave={hoveredBeer}
+                style={{ background: data[1] }}
+                onClick={handleClick}
+              >
+                <Ticker move={tickerOn} className="ticker">
+                  {({ index }) => (
+                    <>
+                      <p>{beer.tagline}</p>
+                    </>
+                  )}
+                </Ticker>
 
-              <div className="beerCard__result">
-                <h1 className="beerCard__result-name">{beer.name}</h1>
-                <img
-                  className="beerCard__result-image"
-                  src={beer.image_url}
-                  alt=""
-                />
-                <p className="beerCard__result-abv">ABV {beer.abv} %</p>
+                <div className="beerCard__result">
+                  <h1 className="beerCard__result-name">{beer.name}</h1>
+                  <img
+                    className="beerCard__result-image"
+                    src={beer.image_url}
+                    alt=""
+                  />
+                  <p className="beerCard__result-abv">ABV {beer.abv} %</p>
+                </div>
               </div>
-            </div> : <p>d</p>}
+            ) : (
+              <div className="beerCardClick" onClick={handleClick}>
+                <div className="beerCardClick__resultClick">
+                  <h5>{beer.name}</h5>
+                  <h6>Release: {beer.first_brewed}</h6>
+                  <p>{beer.description}</p>
+                </div>
+              </div>
+            )}
           </>
         );
       }}
