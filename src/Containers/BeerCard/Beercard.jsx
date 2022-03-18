96de5d73777740f4284acfcefebd571bce38657f
Paul <paulmichaelhardman@gmail.com>
2022-03-18 15:35:42 +0000

    all

--- src/Containers/BeerCard/Beercard.jsx
+++ src/Containers/BeerCard/Beercard.jsx
@@ -3,43 +3,49 @@ import "./Beercard.scss";
 import Ticker from "react-ticker";
 import { Palette } from "color-thief-react";
 const Loading = () => <div>Loading...</div>;
-
 const Beercard = ({ beer }) => {
   const [tickerOn, setTickerOn] = useState(false);
-
   const hoveredBeer = () => {
     setTickerOn(!tickerOn);
+  };
+  const [showText, setShowText] = useState(true);
+  const handleClick = () => {
+    setShowText(!showText);
   };
 
   const imgSrc = beer.image_url;
   return (
-    <Palette src={imgSrc} crossOrigin="anonymous" format="hex" colorCount={4}>
+    <Palette src={imgSrc} crossOrigin="anonymous" format="hex" colorCount={2}>
       {({ data, loading }) => {
         if (loading) return <Loading />;
         return (
-          <div
-            className="beerCard"
-            onMouseEnter={hoveredBeer}
-            onMouseLeave={hoveredBeer}
-            style={{ background: data[1] }}
-          >
-            <Ticker move={tickerOn} className="ticker">
-              {({ index }) => (
-                <>
-                  <p>{beer.tagline}</p>
-                </>
-              )}
-            </Ticker>
-            <div className="beerCard__result">
-              <h1 className="beerCard__result-name">{beer.name}</h1>
-              <img
-                className="beerCard__result-image"
-                src={beer.image_url}
-                alt=""
-              />
-              <p className="beerCard__result-abv">ABV {beer.abv} %</p>
-            </div>
-          </div>
+          <>
+            {showText ? <div
+              className="beerCard"
+              onMouseEnter={hoveredBeer}
+              onMouseLeave={hoveredBeer}
+              style={{ background: data[1] }}
+              onClick ={handleClick}
+            >
+              <Ticker move={tickerOn} className="ticker">
+                {({ index }) => (
+                  <>
+                    <p>{beer.tagline}</p>
+                  </>
+                )}
+              </Ticker>
+
+              <div className="beerCard__result">
+                <h1 className="beerCard__result-name">{beer.name}</h1>
+                <img
+                  className="beerCard__result-image"
+                  src={beer.image_url}
+                  alt=""
+                />
+                <p className="beerCard__result-abv">ABV {beer.abv} %</p>
+              </div>
+            </div> : <p>d</p>}
+          </>
         );
       }}
     </Palette>
